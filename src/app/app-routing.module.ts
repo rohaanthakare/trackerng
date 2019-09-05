31cf3fbc000f1beabf5d17786e0ef66bf3543c89
Rohan Thakare <rohaanthakare@gmail.com>
2019-09-05 16:10:53 +0530

    Nested Routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 const routes: Routes = [{
   path: '',
   component: LoginComponent
+}, {
+  path: 'login',
+  component: LoginComponent
+}, {
+  path: 'home',
+  component: HomeComponent,
+  children:[{
+    path: '',
+    component: DashboardComponent
+  }, {
+    path: 'profile',
+    component: ProfileComponent
+  }]
 }];
 
 @NgModule({
