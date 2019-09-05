983c78eef73bcc9b5e2deaef034045ca8c21ebda
Rohan Thakare <rohaanthakare@gmail.com>
2019-09-05 17:34:04 +0530

    Login page added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,15 @@
+// Core Imports
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+// Angular Material Imports 
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+// Application Imports
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -19,7 +28,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     DashboardComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, BrowserAnimationsModule, FormsModule,
+    MatInputModule, MatButtonModule, MatIconModule,
     AppRoutingModule
   ],
   providers: [],
