32c2968c640e9892b36cc82fcd530cfbc5bed436
Rohan Thakare <rohaanthakare@gmail.com>
2019-09-27 10:31:31 +0530

    Bootstrap changes done for Server deployment

--- Bootstrap.php
+++ Bootstrap.php
@@ -1,8 +1,9 @@
 <?php
+ini_set('display_errors', '1');
 define('ROOT_DIR',__DIR__);
 
-require_once(ROOT_DIR.'./Module/Global/ExceptionHandler.php');
-require_once(ROOT_DIR.'./Module/User/UserController.php');
+require_once(ROOT_DIR.'/Module/Global/ExceptionHandler.php');
+require_once(ROOT_DIR.'/Module/User/UserController.php');
 
 $moduleName = $_REQUEST['Module'];
 $className = $moduleName.'Controller';
