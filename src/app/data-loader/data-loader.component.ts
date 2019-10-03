e1ba28ecc71ca446746975bd0ab5585d2c320300
Rohan Thakare <rohaanthakare@gmail.com>
2019-10-03 18:31:50 +0530

    Load data changes

--- src/app/data-loader/data-loader.component.ts
+++ src/app/data-loader/data-loader.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LoadDataService } from '../services/load-data.service';
+import { DataLoadModule } from '../models/data-load-module.model';
 
 @Component({
   selector: 'app-data-loader',
@@ -7,15 +8,16 @@ import { LoadDataService } from '../services/load-data.service';
   styleUrls: ['./data-loader.component.scss']
 })
 export class DataLoaderComponent implements OnInit {
-
+  moduleForLoading: DataLoadModule[] = [];
   constructor(private loadData: LoadDataService) { }
 
   ngOnInit() {
     this.loadData.getLoadDataConfig().subscribe(
       data => {
         console.log('Inside Data');
-        const tmpdata = new DOMParser();
-        const tmpXML = tmpdata.parseFromString(data, 'text/xml');
+        const xmlParser = new DOMParser();
+        const loadDataXML = xmlParser.parseFromString(data, 'text/xml');
+        loadDataXML.getElementsByTagName('LoadDataModule').length;
         console.log(data);
       },
       error => {
---
+++ src/app/models/data-load-module.model.ts
@@ -0,0 +1,5 @@
+export class DataLoadModule {
+    moduleName: string;
+    dataFileName: string;
+    dataFilePath: string;
+}
