36cebdcd5e5de20b1b037c752f46f0d2565be169
Rohan Thakare <rohaanthakare@gmail.com>
2019-11-13 18:26:32 +0530

    Contact routing added

--- Module/Contact/ContactController.php
+++ Module/Contact/ContactController.php
@@ -1,7 +1,43 @@
 <?php
 Class ContactController {
     public function getUserContacts() {
-        
+        try {
+
+        } catch (Exception $e) {
+            Logger::writeLog('ERROR',get_called_class().' - getUserContacts',$e->getMessage());
+            http_response_code(500);
+            die($e->getMessage());
+        }
+    }
+
+    public function createUserContact() {
+        try {
+
+        } catch (Exception $e) {
+            Logger::writeLog('ERROR',get_called_class().' - createUserContact',$e->getMessage());
+            http_response_code(500);
+            die($e->getMessage());
+        }
+    }
+
+    public function updateUserContact() {
+        try {
+
+        } catch (Exception $e) {
+            Logger::writeLog('ERROR',get_called_class().' - updateUserContact',$e->getMessage());
+            http_response_code(500);
+            die($e->getMessage());
+        }
+    }
+
+    public function deleteUserContact() {
+        try {
+
+        } catch (Exception $e) {
+            Logger::writeLog('ERROR',get_called_class().' - deleteUserContact',$e->getMessage());
+            http_response_code(500);
+            die($e->getMessage());
+        }
     }
 }
 ?>
--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactListComponent } from './contact-list/contact-list.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
+import { Routes, RouterModule } from '@angular/router';
 
-
+const routes: Routes = [{
+  path: '',
+  children: [{
+    path: '',
+    component: ContactListComponent
+  }, {
+    path: 'create',
+    component: ContactFormComponent
+  }, {
+    path: 'edit/:id',
+    component: ContactFormComponent
+  }]
+}];
 
 @NgModule({
   declarations: [ContactListComponent, ContactFormComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class ContactModule { }
