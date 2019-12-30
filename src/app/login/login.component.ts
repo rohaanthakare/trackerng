c44c6eb5030191d8a07e67b31d9c02551aa5d3a6
Rohan Thakare <rohaanthakare@gmail.com>
2019-12-30 22:33:46 +0530

    Error handling updated for login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -37,7 +37,7 @@ export class LoginComponent implements OnInit {
           }
         },
         error => {
-          const errorMsg = (error.error) ? error.error : error.statusText;
+          const errorMsg = (error.error) ? error.error.message : error.statusText;
           this.messageService.showErrorMessage(errorMsg, 'center', 'top');
         }
       );
