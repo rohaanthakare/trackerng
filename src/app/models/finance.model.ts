e9755b42f29d99046579ce5d07adfce9bea30e8e
Rohan Thakare <rohaanthakare@gmail.com>
2020-01-06 19:49:17 +0530

    Branch upload logic added

--- src/app/models/finance.model.ts
+++ src/app/models/finance.model.ts
@@ -2,3 +2,15 @@ export class Bank {
     bankCode: string;
     bankName: string;
 }
+
+export class Branch {
+    branchCode: string;
+    branchName: string;
+    branchIfsc: string;
+    branchMicr: string;
+    address: string;
+    city: string;
+    state: string;
+    country: string;
+    bank: string;
+}
