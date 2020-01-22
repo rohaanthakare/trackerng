3caeac1b53b651d6a9e0b52f2c450f9a5340af00
Rohan Thakare <rohaanthakare@gmail.com>
2020-01-22 23:24:19 +0530

    Deposit form initial changes

--- src/app/finance/deposit-form/deposit-form.component.ts
+++ src/app/finance/deposit-form/deposit-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-deposit-form',
@@ -6,8 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./deposit-form.component.scss']
 })
 export class DepositFormComponent implements OnInit {
-
-  constructor() { }
+  depositTypes = [];
+  isDepositTypeLoaded = false;
+  accounts = [];
+  isAccountsLoaded = false;
+  transactionSubCategories = [];
+  isTransactionSubCategoriesLoaded = false;
+  depositTypeCtrl = new FormControl();
+  accountCtrl = new FormControl();
+  amountCtrl = new FormControl();
+  depositSubCategoryCtrl = new FormControl();
+  transactionDateCtrl = new FormControl();
+  transactionDetailCtrl = new FormControl();
+  depositForm: FormGroup = this.formBuilder.group({
+    transactionSubCategory: this.depositSubCategoryCtrl,
+    account: this.accountCtrl,
+    amount: this.amountCtrl,
+    depositType: this.depositTypeCtrl,
+    transactionDate: this.transactionDateCtrl,
+    transactionDetail: this.transactionDetailCtrl
+  });
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
   }
