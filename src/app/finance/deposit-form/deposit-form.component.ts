e74869a9bd894647047ef77a4e27c084531bdafd
Rohan Thakare <rohaanthakare@gmail.com>
2020-01-23 17:01:36 +0530

    Deposit form fields added

--- src/app/finance/deposit-form/deposit-form.component.ts
+++ src/app/finance/deposit-form/deposit-form.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { MasterDataService } from 'src/app/services/master-data.service';
+import { ModelFormComponent } from 'src/app/core/model-form/model-form.component';
 
 @Component({
   selector: 'app-deposit-form',
@@ -7,6 +9,7 @@ import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./deposit-form.component.scss']
 })
 export class DepositFormComponent implements OnInit {
+  @ViewChild(ModelFormComponent, {static: false}) modelForm: ModelFormComponent;
   depositTypes = [];
   isDepositTypeLoaded = false;
   accounts = [];
@@ -27,9 +30,64 @@ export class DepositFormComponent implements OnInit {
     transactionDate: this.transactionDateCtrl,
     transactionDetail: this.transactionDetailCtrl
   });
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private masterDataService: MasterDataService) { }
 
   ngOnInit() {
+    this.masterDataService.getMasterDataForParent('DEPOSIT_TYPE').subscribe(
+      (response: any) => {
+        this.depositTypes = response.data;
+        this.isDepositTypeLoaded = true;
+        this.allDataLoaded();
+      }
+    );
+
+    this.masterDataService.getMasterDataForParent('DEPOSIT_CATEGORY').subscribe(
+      (response: any) => {
+        this.transactionSubCategories = response.data;
+        this.isTransactionSubCategoriesLoaded = true;
+        this.allDataLoaded();
+      }
+    );
+  }
+
+  allDataLoaded() {
+    if (this.isDepositTypeLoaded && this.isTransactionSubCategoriesLoaded) {
+      this.modelForm.setFieldConfigs(this.getFieldConfigs());
+    }
+  }
+
+  getFieldConfigs() {
+    return [{
+      label: 'Type',
+      name: 'depositType',
+      type: 'select',
+      dataScource: this.depositTypes,
+      valueField: '_id',
+      displayField: 'configName',
+      control: this.depositTypeCtrl,
+      controlName: 'depositType'
+    }, {
+      label: 'Amount',
+      name: 'amount',
+      type: 'number',
+      control: this.amountCtrl,
+      controlName: 'amount'
+    }, {
+      label: 'Date',
+      name: 'transactionDate',
+      type: 'date',
+      control: this.transactionDateCtrl,
+      controlName: 'transactionDate'
+    }, {
+      label: 'Category',
+      name: 'transactionSubCategory',
+      type: 'select',
+      dataScource: this.transactionSubCategories,
+      valueField: '_id',
+      displayField: 'configName',
+      control: this.depositSubCategoryCtrl,
+      controlName: 'transactionSubCategory'
+    }];
   }
 
 }
